a12345855c83765cfde2811f1dc93ae70c1aa933
PPPatriccccc <69126356+PPPatriccccc@users.noreply.github.com>
2020-08-21 00:26:37 -0400

    Add files via upload

---
+++ text adding paragraph.py
@@ -0,0 +1,26 @@
+from docx import Document
+
+# acquire text location
+location = str(input('Please enter file location:'))
+# location = '/Users/patrickluo/Documents/Coding/abc.docx'
+# location = 'abc.docx'
+doc = Document(location)
+
+# acquire target text and replacing text
+search_text =str(input('Please enter target text:'))
+replace_text =str(input('Please enter replace text:'))
+
+# loop
+for paragraph in doc.paragraphs:
+    # print(paragraph)
+    for run in paragraph.runs:
+        # print(run.text)
+        if run.text.find(search_text) != -1:
+            
+            text = run.text
+            text = text.replace(search_text,'; \n')
+            # print(text)
+            run.text = text
+
+
+doc.save(location)
